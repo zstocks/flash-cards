d551ab91ac079288f41b8b0dd98d9b4d02a366c3
Zachary Stocks <stocks.zachary@gmail.com>
2022-04-11 20:09:00 -0600

    Add a couple simple routes

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
- response.send('I Love Treehouse!');
+app.get('/', (req, res) => {
+ res.send('<h1>I Love Treehouse!</h1>');
 });
 
-app.listen(3000);
+app.get('/hello', (req, res) => {
+ res.send('<h1>Hello, JavaScript Developer!</h1>');
+});
+
+app.listen(3000, () => {
+ console.log('The application is running on localhost:3000')
+});
