000c6e0957fc5230323a6f4edd8a2a9a8be4d4e8
Zachary Stocks <stocks.zachary@gmail.com>
2022-04-19 10:48:15 -0600

    adds error handling to the app.

--- app.js
+++ app.js
@@ -9,14 +9,33 @@ app.use(cookieParser());
 // Define the view engine to use in the project
 app.set('view engine', 'pug');
 
+// Middleware
+app.use((req, res, next) => { 
+    console.log('Hello');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('World');
+    next();
+});
+
+
+
+
 // Define Routes
+// GET
 app.get('/', (req, res) => {
+
     const name = req.cookies.username;
+
+    // Only display the / route if the username cookie has been set
     if (name) {
         res.render('index', { name });
     } else { 
         res.redirect('/hello');
     }
+
 });
 
 app.get('/cards', (req, res) => {
@@ -26,14 +45,19 @@ app.get('/cards', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
+
     const name = req.cookies.username;
+
+    // Only display the hello template if username is *NOT* set
     if (name) {
         res.redirect('/');
     } else {
         res.render('hello');
     }
+
 });
 
+//POST
 app.post('/hello', (req, res) => {
     res.cookie('username', req.body.username);
     res.redirect('/');
@@ -42,6 +66,18 @@ app.post('/hello', (req, res) => {
 app.post('/goodbye', (req, res) => {
     res.clearCookie('username');
     res.redirect('/hello');
+});
+
+app.use((req, res, next) => { 
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => { 
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
 });
 
 // Define server
