44541c99734d0e7cf283f722e6fe5dacfae28c4b
Zachary Stocks <stocks.zachary@gmail.com>
2022-04-20 15:23:34 -0600

    adds card data and display logic

--- routes/cards.js
+++ routes/cards.js
@@ -1,10 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const { data } = require('../data/flashcardData.json');
+const { cards } = data;
 
-router.get('/', (req, res) => {
-    res.locals.prompt = "Who is buried in Grant's tomb?";
-    res.locals.hint = "Think about whose tomb it is.";
-    res.render('card');
+router.get('/:id', (req, res) => {
+    const { side } = req.query;
+    const { id } = req.params;
+
+    if (!side) {
+        return res.redirect(`/cards/${id}?side=question`);
+    };
+
+    const name = req.cookies.username;
+    const text = cards[id][side];
+    const { hint } = cards[id];
+    const templateData = { id, text, name };
+
+    if (side === 'question') {
+        templateData.hint = hint;
+        templateData.flipLink = `answer`;
+        templateData.flipText = 'Answer';
+    } else if (side === 'answer') {
+        templateData.flipLink = `question`;
+        templateData.flipText = 'Question';
+    } else { 
+        return res.redirect(`/cards/${id}?side=question`);
+    }
+    
+    res.render('card', templateData);
 });
+
+router.get('/', (req, res) => { 
+    // Write logic to find a random card
+    const randomCard = Math.floor(Math.random() * cards.length);
+    
+    // Redirect users to view that card
+    res.redirect(`/cards/${randomCard}`);
+});
+
+
 
 module.exports = router;
